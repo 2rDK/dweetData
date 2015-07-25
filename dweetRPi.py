41dd4ebf1bb3e18cfb5399f67f4668d7e654b334
2rDK <lemminger@gmail.com>
2015-07-25 14:38:34 +0200

    Bedre fejlhåndtering ved eksekvering på ikke-linux maskine

--- dweetRPi.py
+++ dweetRPi.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python
 import requests
-import random
-from time import sleep
-from readSensors import readSensors
-from readCPULoad import readCPULoad
+from fcn.readSensors import readRPiCPUTemp
+from fcn.readCPULoad import readCPULoad
 
 myKey1 = "CPU_temp"
 myKey2 = "CPU_load"
 myName = "HusetIO"
 dweetIO = "https://dweet.io/dweet/for/"
 
-temp = readSensors()
+temp = readRPiCPUTemp()
 loadCPU = readCPULoad()
-rqsString = dweetIO + myName + '?' + myKey1 + '=' + str(temp) + '&' + myKey2 + $
+rqsString = dweetIO + myName + '?' + myKey1 + '=' + str(temp) + '&' + myKey2 + '=' + str(loadCPU)
 
 print(rqsString)
 
