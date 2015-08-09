5465c4c92eaf912891c532f428b2cb1c0e3ca90b
2rDK <lemminger@gmail.com>
2015-08-09 10:16:13 +0200

    corrected import

--- dweetRPi.py
+++ dweetRPi.py
@@ -3,7 +3,7 @@ from fcn.readSensors import readRPiCPUTemp
 from fcn.readCPULoad import readCPULoad
 from fcn.dweetTools import dweetSender
 #from fcn.mySqlTools import mySqlSender
-import fcn.HTU21D
+from fcn.HTU21D import HTU21D
 
 
 myKeys = {
