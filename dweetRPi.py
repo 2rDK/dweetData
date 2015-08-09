f747ea4d93d69d6f83ae2aa8462eb504b241e960
2rDK <lemminger@gmail.com>
2015-08-09 10:12:25 +0200

    test

--- dweetRPi.py
+++ dweetRPi.py
@@ -2,18 +2,20 @@
 from fcn.readSensors import readRPiCPUTemp
 from fcn.readCPULoad import readCPULoad
 from fcn.dweetTools import dweetSender
-from fcn.mySqlTools import mySqlSender
+#from fcn.mySqlTools import mySqlSender
+import fcn.HTU21D
 
 
 myKeys = {
           'CPU_temp': readRPiCPUTemp(), 
-          'CPU_load': readCPULoad()
+          'CPU_load': readCPULoad(),
+          'Loft_temp': HTU21D.read_temperature()
           }
 
 myName = "HusetIO_RPi"
 
 print(dweetSender(myName,myKeys))
 
-mySqlSender("CPU_temp",15,1)
+#mySqlSender("CPU_temp",15,1)
 
     
